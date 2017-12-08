a182004ef7aa14cee4648dbe948b31ef4b615ab1
liming <cqupt_hm@163.com>
2017-12-08 10:37:17 +0800

    21题

--- src/Easy.java
+++ src/Easy.java
@@ -10,4 +10,39 @@ public class Easy {
 	public int missingNumber(int[] nums) {
 		return 0;
 	}
+
+	/**
+	 * 21
+	 */
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	/**
+	 * 21
+	 */
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode preHead = new ListNode(-1);
+
+		ListNode pre = preHead;
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				pre.next = l1;
+				l1 = l1.next;
+			} else {
+				pre.next = l2;
+				l2 = l2.next;
+			}
+			pre = pre.next;
+		}
+
+		pre.next = l1 == null ? l2 : l1;
+		return preHead.next;
+	}
+
 }
